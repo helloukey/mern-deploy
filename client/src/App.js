6007d8d2afd59801a2349e7d0dc003ba7628c92b
helloukey <kunalukey32@gmail.com>
2023-03-16 15:39:25 +0530

    added backend api for frontend.

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ function App() {
   const [message, setMessage] = useState("");
 
   useEffect(() => {
-    fetch("http://localhost:4000")
+    fetch("https://mern-deploy-backend.onrender.com")
       .then((res) => res.json())
       .then((data) => setMessage(data.message));
   },[]);
